a2cf79fb80310e19d24f91e5f068b9809e7f079b
pinkWar123 <nquan003@gmail.com>
2025-02-16 22:42:01 +0700

    Refactor to bring all states into App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,69 @@
 import './App.css'
 import StudentList from './components/StudentList'
+import FacultyList from './components/Faculty'
+import { useEffect, useState } from 'react';
+import IStudent from './models/Student';
+import IProgram from './models/Program';
+import IStatus from './models/Status';
+import { callGetAllStudents } from './services/student';
+import { callGetStudyPrograms } from './services/studyProgram';
+import { callGetAllStatuses } from './services/status';
+import IFaculty from './models/Faculty';
+import Program from './components/Program';
+import Status from './components/Status';
 function App() {
+  const [students, setStudents] = useState<IStudent[]>([]);
+  const [studyPrograms, setStudyPrograms] = useState<IProgram[]>([]);
+  const [statuses, setStatuses] = useState<IStatus[]>([]);
+  const [faculties, setFaculties] = useState<IFaculty[]>([]);
+  useEffect(() => {
+    const fetchStudents = async () => {
+        try {
+            const response = await callGetAllStudents();
+            setStudents(response);
+        } catch (error) {
+            console.error('Error fetching students:', error);
+        }
+    };
+    fetchStudents();
+  }, []);
 
+  useEffect(() => {
+      const fetchStudyPrograms = async () => {
+          try {
+              const response = await callGetStudyPrograms();
+              setStudyPrograms(response);
+          } catch (error) {
+              console.error('Error fetching study programs:', error);
+          }
+      };
+      fetchStudyPrograms();
+  }, []);
+
+  useEffect(() => {
+      const fetchStatuses = async () => {
+          try {
+              const response = await callGetAllStatuses();
+              setStatuses(response);
+          } catch (error) {
+              console.error('Error fetching statuses:', error);
+          }
+      };
+      fetchStatuses();    
+  }, [])
+  const updateStudents = async () => {
+    const students = await callGetAllStudents();
+    setStudents(students);
+  }
+
+
+  
   return (
     <>
-      <StudentList />
+      <StudentList students={students} studyPrograms={studyPrograms} statuses={statuses} faculties={faculties} updateStudents={updateStudents} />
+      <FacultyList faculties={faculties} updateFaculties={setFaculties}/>
+      <Program programs={studyPrograms} updatePrograms={setStudyPrograms}/>
+      <Status statuses={statuses} updateStatuses={setStatuses}/>
     </>
   )
 }
